fc9e05ee2536c7d62a20218fc8a0002256331855
pranaliPukale <pukalepranali@gmail.com>
2022-10-04 07:01:43 -0700

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("hello");
+console.log("My name is Pranali");
+console.info('this is info');
+console.warn("I am not bad guy");
+console.error("failed to executes");
---
+++ homeWork1.js
@@ -0,0 +1,6 @@
+console.log("My name is Pranali Nandkishor Pukale");
+console.log("My hobbies are reading book,cooking,gardening");
+console.info("My Personal Strength are positive attitude,self motivated,state forward approach");
+console.warn("My weakness are I can concentrate only one thing at a time");
+console.log(30+50);
+console.log(49*479);
