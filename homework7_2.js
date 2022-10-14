e80d48d0777d5ae074b40f7e2eefe20f1259d91e
pranaliPukale <pukalepranali@gmail.com>
2022-10-14 04:39:28 -0700

    for loop
    for loop

---
+++ homework7_2.js
@@ -0,0 +1,66 @@
+console.log("=========Question 1============");
+count=0
+for(i=5;i<100;i=i+5){
+    //console.log(i);
+    count++;
+    if (count==10) {
+        break;
+    }
+
+}
+console.log("Table of 5 is done");
+console.log("=========Question 2============");
+count=0
+for(i=10;i<=100;i=i+10){
+   // console.log(i);
+    count++;
+    if (count==10) {
+        break;
+    }
+
+}
+console.log("Table of 10 is done");
+console.log("=========Question 3============");
+count=0
+for(i=100;i>=10;i=i-10){
+   //console.log(i);
+    count++;
+    if (count==10) {
+        break;
+    }
+
+}
+console.log("Reverse Table of 10 is done");
+console.log("=========Question 4============");
+
+for(i=50;i>=40;i=i-1){
+   //console.log(i);
+
+}
+console.log("Reverse 50 to 40  is done");
+console.log("=========Question 5============");
+for(i=5;i<=15;i++){
+//console.log(i);
+}
+console.log('5 to 15 increment by 1 is done');
+console.log("=========Question 6============");
+var count=0;
+for(i=1;i<=50;i=i+2){
+   // console.log(i  );
+    count++;
+    if(count==15){
+        break;
+    }
+}
+console.log("First 15 odd number is done");
+console.log("=========Question 7============");
+var count=0;
+for(i=2;i<=50;i=i+2){
+    //console.log(i  );
+    count++;
+    if(count==10){
+        break;
+    }
+}
+console.log("First 15 odd number is done");
+
